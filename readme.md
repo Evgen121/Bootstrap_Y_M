761ebd482affd6928405d616543d54154731e5fd
Yevhen Malyi <malyje164@gmail.com>
2022-07-03 01:23:31 +0200

    first commit

--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
-# Git and Github Cours
+This site uses the live search feature on the site,
+I honestly found it on the Internet, and applied it to my site.
+Thank you for your attention .
+Have a good day
 
 Crash course for Git
--- script .js
+++ script .js
@@ -1,1 +1,39 @@
-console.log("Hello world")
+document.querySelector('#search').oninput = function() {
+
+        /* get value from input 
+        the search goes all over the site in paragraphs*/
+
+        let val = this.value.trim();
+        let searchItems = document.querySelectorAll('body p');
+
+        /* input check */
+
+        if (val != '') {
+            searchItems.forEach(function(elem) {
+
+
+                if (elem.innerText.search(val) == -1) {
+                    elem.classList.add('hide');
+                    elem.innerHTML = elem.innerText;
+                } else {
+                    /*   loop through all elements in body paragraphs */
+                    elem.classList.remove('hide');
+                    let str = elem.innerText;
+                    elem.innerHTML = inserMark(str, elem.innerText.search(val), val.length);
+                }
+            })
+        } else { /*  hide unwanted elements */
+            searchItems.forEach(function(elem) {
+                elem.classList.remove('hide');
+                elem.innerHTML = elem.innerText;
+            })
+
+        }
+        console.log(val)
+    }
+    /*mark found material */
+
+function inserMark(string, pos, len) {
+    return string.slice(0, pos) + "<mark>" + string.slice(pos, pos + len) +
+        "</mark>" + string.slice(pos + len);
+}
